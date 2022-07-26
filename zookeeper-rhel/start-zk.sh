bd92eb80795862b16ac16be3291ea61d5aaf3897
C Wills <caron.wills@gmail.com>
2022-07-26 22:31:29 +0100

    Initial modified commit

---
+++ zookeeper-rhel/start-zk.sh
@@ -0,0 +1,4 @@
+#sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
+sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
+
+/opt/apache-zookeeper-3.8.0-bin/bin/zkServer.sh start-foreground
